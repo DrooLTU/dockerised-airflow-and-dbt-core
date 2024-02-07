e69f784540b45649047b2366a3d57b06b85e631b
DrooLTU <dzastinaslt@gmail.com>
2024-02-07 16:33:47 +0200

    Initial setup for service account, WIP

--- main.tf
+++ main.tf
@@ -18,3 +18,38 @@ resource "google_bigquery_dataset" "raw_cmc_data" {
   dataset_id = "raw_cmc_data"
   project    = var.project_id
 }
+
+resource "google_service_account" "system_service_account" {
+  account_id   = "system-service-account"
+  display_name = "System Service Account"
+}
+
+resource "google_project_iam_member" "service_account_member" {
+  project = var.project_id
+  role    = "roles/editor"
+
+  member = "serviceAccount:${google_service_account.system_service_account.email}"
+}
+
+resource "google_project_iam_member" "bigquery_role" {
+  project = var.project_id
+  role    = "roles/bigquery.dataEditor"
+
+  member = "serviceAccount:${google_service_account.system_service_account.email}"
+}
+
+resource "google_project_iam_member" "storage_role" {
+  project = var.project_id
+  role    = "roles/storage.objectAdmin"
+
+  member = "serviceAccount:${google_service_account.system_service_account.email}"
+}
+
+resource "google_service_account_key" "system_service_account_key" {
+  service_account_id = google_service_account.system_service_account.name
+}
+
+output "service_account_credentials" {
+  value = google_service_account_key.system_service_account_key.private_key
+  sensitive = true
+}
