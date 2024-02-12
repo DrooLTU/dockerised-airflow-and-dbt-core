d305c7439d0a4bb4c36ae6fba1bab9f57931e8a2
DrooLTU <dzastinaslt@gmail.com>
2024-02-12 10:18:08 +0200

    Basic incremental load implemented

--- dbt/cmc_elt/models/staging/cmc/stg_crypto_price.sql
+++ dbt/cmc_elt/models/staging/cmc/stg_crypto_price.sql
@@ -2,6 +2,9 @@ with
 
 source as (
     select * from {{ source('raw_cmc_data', 'raw')}}
+    {% if is_incremental() %}
+        where last_updated > (select max(last_updated) from {{ this }})
+    {% endif %}
 )
 
 select * from source
