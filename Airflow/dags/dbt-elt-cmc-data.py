bd6e0d5c498eac160f58711c1c16af7b7099c46a
DrooLTU <dzastinaslt@gmail.com>
2024-02-12 13:38:09 +0200

    Preparations for docker operator

--- Airflow/dags/dbt-elt-cmc-data.py
+++ Airflow/dags/dbt-elt-cmc-data.py
@@ -4,6 +4,9 @@ from airflow.models import Variable
 from airflow.exceptions import AirflowException
 from airflow.decorators import dag, task
 
+from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import Mount
+
 import pendulum
 
 START = pendulum.datetime(2024, 2, 8, tz="UTC")
@@ -19,7 +22,18 @@ def dbt_cmc_elt():
         """
         Executes 'dbt run'
         """
-
+        fetch_data_task = DockerOperator(
+        task_id="fetch_data_task",
+        image="justinaslorjus/kaggle_fetch_dataset:1.0-3.11",
+        trigger_rule="none_failed",
+        command=[
+        ],
+        auto_remove=True,
+        mount_tmp_dir=False,
+        mounts=[
+            Mount(source=VOLUME_NAME, target="/data", type="volume"),
+        ],
+    )
         return
     
     return
