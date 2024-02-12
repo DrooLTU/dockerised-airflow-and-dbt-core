b77f342bf6531bd475f7113789f4b6e86021e7b8
DrooLTU <dzastinaslt@gmail.com>
2024-02-12 19:37:01 +0200

    Cred file sync, adjustmens on airflow image to run DockerOperator

--- Airflow/dags/dbt-elt-cmc-data.py
+++ Airflow/dags/dbt-elt-cmc-data.py
@@ -7,10 +7,12 @@ from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
 
 import pendulum
+import shutil
 
 START = pendulum.datetime(2024, 2, 8, tz="UTC")
 PROJECT_NAME = os.environ.get('COMPOSE_PROJECT_NAME') 
-VOLUME_NAME = f"{PROJECT_NAME}_shared-creds-volume"
+script_path = os.path.abspath(__file__)
+project_directory = os.path.abspath(os.path.join(script_path, '..', '..'))
 
 @dag(dag_id='dbt-cmc-elt', start_date=START, schedule="@daily", catchup=False)
 def dbt_cmc_elt():
@@ -19,24 +21,40 @@ def dbt_cmc_elt():
     """
 
     @task()
+    def copy_file(source_path: str, destination_path: str):
+        """
+        This has to handle shared creds.
+        """
+        try:
+            shutil.copy(source_path, destination_path)
+            print(f"File copied from {source_path} to {destination_path}")
+        except Exception as e:
+            print(f"Error copying file: {e}")
+
+
+    @task()
     def dbt_run():
         """
         Executes 'dbt run'
         """
-        dbt_run_task = DockerOperator(
+
+        operator = DockerOperator(
             task_id="dbt_run_task",
-            image="justinaslorjus/kaggle_fetch_dataset:1.0-3.11",
+            image="justinaslorjus/dbt_cmc_elt:0.1",
             trigger_rule="none_failed",
-            command=[],
+            command=["docs serve"],
             auto_remove=True,
             mount_tmp_dir=False,
             mounts=[
-                Mount(source=VOLUME_NAME, target="/shared_creds", type="volume"),
+                Mount(source=f"{PROJECT_NAME}_shared-creds-volume", target="/shared_creds", type="volume"),
             ],
         )
 
-        return
+        operator.execute(context=None)
     
-    return
+    copy_task = copy_file('/opt/airflow/config/shared_creds/turing-project-m3-s4-1591ea509586.json', '/opt/airflow/shared/gcp_default.json')
+    dbt_run_task = dbt_run()
+
+    copy_task >> dbt_run_task
 
 dbt_cmc_elt()
